8659313ef7cd1251436084fc85b55065b3cf54c3
Emmabee-initiatives <138867283+Emmabee-initiatives@users.noreply.github.com>
2023-10-19 01:39:36 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
-function showmenu()
+let  Navlinks = document.getElementById("NavLinks");
+      
+        
+        
+        function showmenu(){
+            Nav-links.learn.right = "-200px";
+            }
+        
+        function hidemenu(){
+            Nav-links.learn.right = "0";
+        }
+        
